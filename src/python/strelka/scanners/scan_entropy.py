ad56de4be257e2e02c0609dfe7480bbda7e8009c
ibrahim <mohamed.samir@dphish.com>
2026-08-03 21:48:36 +0300

    this for entropy

--- src/python/strelka/scanners/scan_entropy.py
+++ src/python/strelka/scanners/scan_entropy.py
@@ -1,3 +1,5 @@
+import logging
+
 import entropy
 
 from strelka import strelka
@@ -7,4 +9,5 @@ class ScanEntropy(strelka.Scanner):
     """Calculates entropy of files."""
 
     def scan(self, data, file, options, expire_at):
+        logging.debug(f"Calculating entropy for data: {data}")
         self.event["entropy"] = entropy.shannon_entropy(data)
