fede196c8b09a5dfbb89ad77badfc8f16524897c
ibrahim <mohamed.samir@dphish.com>
2026-08-03 21:57:50 +0300

    this for entropy 2

--- src/python/strelka/scanners/scan_entropy.py
+++ src/python/strelka/scanners/scan_entropy.py
@@ -1,6 +1,8 @@
 import logging
 
 import entropy
+import validators
+from tldextract import TLDExtract
 
 from strelka import strelka
 
@@ -8,6 +10,20 @@ from strelka import strelka
 class ScanEntropy(strelka.Scanner):
     """Calculates entropy of files."""
 
+    def init(self):
+        self.extract = TLDExtract(suffix_list_urls=[])
+
     def scan(self, data, file, options, expire_at):
-        logging.debug(f"Calculating entropy for data: {data}")
-        self.event["entropy"] = entropy.shannon_entropy(data)
+        entropy_data = data
+
+        try:
+            decoded = data.decode().strip()
+            if validators.url(decoded):
+                domain = self.extract(decoded).domain
+                if domain:
+                    entropy_data = domain.encode()
+        except UnicodeDecodeError:
+            pass
+
+        logging.debug(f"Calculating entropy for data: {entropy_data}")
+        self.event["entropy"] = entropy.shannon_entropy(entropy_data)
